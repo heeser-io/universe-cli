a25c428aadcc30621ea1b180cd6628934ae7fbf1
Yann Heeser <yann@heeser.io>
2022-05-27 00:39:36 +0200

    Force push

--- cmd/stack/push.go
+++ cmd/stack/push.go
@@ -6,16 +6,21 @@ import (
 )
 
 var (
+	Force   bool
 	PushCmd = &cobra.Command{
 		Use:   "push",
 		Short: "pushes the current stack",
 		Run: func(cmd *cobra.Command, args []string) {
 			v := builder.HasChange()
 
-			if v {
+			if v || Force {
 				builder.BuildStack()
 				builder.Verify()
 			}
 		},
 	}
 )
+
+func init() {
+	PushCmd.Flags().BoolVarP(&Force, "force", "f", false, "force push")
+}
